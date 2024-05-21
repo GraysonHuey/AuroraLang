d9fd13d8f0bd3ac5417c71feb03fd9baf3a3f72b
Grayson Huey <96308751+GraysonHuey@users.noreply.github.com>
2024-05-21 12:22:21 -0500

    Getting closer to fully porting to C++

--- src/(INCOMPLETE) AuroraInterpreter.cpp
+++ src/(INCOMPLETE) AuroraInterpreter.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <variant> // C++ 17 or up needed (replit uses C++ 16)
+#include <variant>
+#include <limits>
 
 // Function to split a string by space
 std::vector<std::string> split(const std::string& s) {
@@ -17,7 +18,14 @@ std::vector<std::string> split(const std::string& s) {
 }
 
 
-int main([[maybe_unused]] int argc, char** argv) {
+int main(int argc, char** argv) {
+
+    if (argc != 2) {
+        std::stringstream ss;
+        ss << "Incorrect arguments! Usage: " << argv[0] << " <filename>\n";
+        throw std::runtime_error(ss.str());
+    }
+
     std::string file_name = argv[1];
 
     if (file_name.size() < 4 || file_name.substr(file_name.size() - 4) != ".aur") {
@@ -88,7 +96,7 @@ int main([[maybe_unused]] int argc, char** argv) {
             std::ostringstream oss;
 
             // Join the parts starting from index 1 with space separator
-            for (size_t i = 1; i < command_parts.size(); ++i) {
+            for (size_t i = 1; i < command_parts.size(); i++) {
                 if (i > 1) {
                     oss << " "; // Add space separator
                 }
@@ -211,14 +219,14 @@ int main([[maybe_unused]] int argc, char** argv) {
         }
 
         else if (command == "DISPLAY") {
-            if (stack.empty()) {
-                throw std::runtime_error("Stack is empty, cannot DISPLAY");
-            }
             if (command_parts.size() == 1 || command_parts[1].find_first_not_of(" \t\r\n") == std::string::npos) {
                 std::cout << std::endl;
             }
             std::string argument = command_parts[1];
             if (argument == "TOP") {
+                if (stack.empty()) {
+                    throw std::runtime_error("Stack is empty, cannot DISPLAY");
+                }
                 try {
                     std::cout << std::get<int>(stack.back()) << std::endl;
                 } catch (std::exception &e) {
@@ -260,7 +268,7 @@ int main([[maybe_unused]] int argc, char** argv) {
             }
             auto item1 = stack[stack.size() - 1];
             auto item2 = stack[stack.size() - 2];
-            bool equal = false;
+            bool equal;
             try {
                 if (std::holds_alternative<int>(item1) && std::holds_alternative<int>(item2)) {
                     equal = std::get<int>(item1) == std::get<int>(item2);
@@ -281,7 +289,7 @@ int main([[maybe_unused]] int argc, char** argv) {
             }
             auto item1 = stack[stack.size() - 1];
             auto item2 = stack[stack.size() - 2];
-            bool equal = true;
+            bool equal;
             try {
                 if (std::holds_alternative<int>(item1) && std::holds_alternative<int>(item2)) {
                     equal = std::get<int>(item1) == std::get<int>(item2);
@@ -294,6 +302,203 @@ int main([[maybe_unused]] int argc, char** argv) {
                 throw std::runtime_error("Bad variant access");
             }
             stack.emplace_back(equal ? 0 : 1);
+        }
+
+        else if (command == "GT") {
+            if (stack.size() < 2) {
+                throw std::runtime_error("Not enough values on the stack to perform GT");
+            }
+            auto item1 = stack[stack.size() - 1];
+            auto item2 = stack[stack.size() - 2];
+            bool greater;
+            try {
+                if (std::holds_alternative<int>(item1) && std::holds_alternative<int>(item2)) {
+                    greater = std::get<int>(item1) > std::get<int>(item2);
+                } else if (std::holds_alternative<std::string>(item1) && std::holds_alternative<std::string>(item2)) {
+                    greater = std::get<std::string>(item1) > std::get<std::string>(item2);
+                } else {
+                    greater = false;
+                }
+            } catch (std::exception &e) {
+                throw std::runtime_error("Bad variant access");
+            }
+            stack.emplace_back(greater ? 1 : 0);
+        }
+
+        else if (command == "LT") {
+            if (stack.size() < 2) {
+                throw std::runtime_error("Not enough values on the stack to perform LT");
+            }
+            auto item1 = stack[stack.size() - 1];
+            auto item2 = stack[stack.size() - 2];
+            bool less;
+            try {
+                if (std::holds_alternative<int>(item1) && std::holds_alternative<int>(item2)) {
+                    less = std::get<int>(item1) < std::get<int>(item2);
+                } else if (std::holds_alternative<std::string>(item1) && std::holds_alternative<std::string>(item2)) {
+                    less = std::get<std::string>(item1) < std::get<std::string>(item2);
+                } else {
+                    less = false;
+                }
+            } catch (std::exception &e) {
+                throw std::runtime_error("Bad variant access");
+            }
+            stack.emplace_back(less ? 1 : 0);
+        }
+
+        else if (command == "AND") {
+            if (stack.size() < 2) {
+                throw std::runtime_error("Not enough values on the stack to perform AND");
+            }
+            auto item1 = stack[stack.size() - 1];
+            auto item2 = stack[stack.size() - 2];
+            try {
+                if (std::holds_alternative<int>(item1) && std::holds_alternative<int>(item2)) {
+                    stack.emplace_back(std::get<int>(item1) == 1 && std::get<int>(item2) == 1 ? 1 : 0);
+                } else {
+                    throw std::runtime_error("Cannot perform AND on an INT and a STR");
+                }
+            } catch (std::bad_variant_access &e) {
+                throw std::runtime_error("Bad variant access");
+            }
+        }
+
+        else if (command == "OR") {
+            if (stack.size() < 2) {
+                throw std::runtime_error("Not enough values on the stack to perform OR");
+            }
+            auto item1 = stack[stack.size() - 1];
+            auto item2 = stack[stack.size() - 1];
+            try {
+                if (std::holds_alternative<int>(item1) && std::holds_alternative<int>(item2)) {
+                    stack.emplace_back(std::get<int>(item1) != 0 || std::get<int>(item2) != 0 ? 1 : 0);
+                } else {
+                    throw std::runtime_error("Cannot perform OR on an INT and a STR");
+                }
+            } catch (std::bad_variant_access &e) {
+                throw std::runtime_error("Bad variant access");
+            }
+        }
+
+        else if (command == "NOT") {
+            if (stack.empty()) {
+                throw std::runtime_error("Stack is empty, cannot perform NOT");
+            }
+            auto item = stack[stack.size() - 1];
+            if (std::holds_alternative<int>(item)) {
+                int item_i = std::get<int>(item) * -1;
+                stack.emplace_back(item_i);
+            } else {
+                throw std::runtime_error("Cannot perform NOT on a STR");
+            }
+        }
+
+        else if (command == "INC") {
+            if (stack.empty()) {
+                throw std::runtime_error("Stack is empty, cannot increment");
+            }
+            try {
+                auto item = stack[stack.size() - 1];
+                if (std::holds_alternative<int>(item)) {
+                    int num = std::get<int>(item);
+                    num++;
+                    stack.pop_back();
+                    stack.emplace_back(num);
+                } else {
+                    throw std::runtime_error("Cannot increment a STR");
+                }
+            } catch (std::bad_variant_access &e) {
+                throw std::runtime_error("Bad variant access");
+            }
+        }
+
+        else if (command == "DEC") {
+            if (stack.empty()) {
+                throw std::runtime_error("Stack is empty, cannot decrement");
+            }
+            try {
+                auto item = stack[stack.size() - 1];
+                if (std::holds_alternative<int>(item)) {
+                    int num = std::get<int>(item);
+                    num--;
+                    stack.pop_back();
+                    stack.emplace_back(num);
+                } else {
+                    throw std::runtime_error("Cannot decrement a STR");
+                }
+            } catch (std::bad_variant_access &e) {
+                throw std::runtime_error("Bad variant access");
+            }
+        }
+
+        else if (command == "READINT") {
+            std::string input;
+            std::cin >> input;
+            try {
+                int input_i = stoi(input);
+                stack.emplace_back(input_i);
+                // Clear the newline character left in the input buffer
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            } catch (std::exception &e) {
+                throw std::runtime_error("READINT only accepts integers. Did you mean to use READSTR?");
+            }
+        }
+
+        else if (command == "READSTR") {
+            std::string input;
+            std::getline(std::cin, input);
+            stack.emplace_back(input);
+            // Clear the newline character left in the input buffer
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
+        else if (command == "SWAP") {
+            auto temp = stack[stack.size() - 1];
+            stack[stack.size() - 1] = stack[stack.size() - 2];
+            stack[stack.size() - 2] = temp;
+        }
+
+        else if (command == "JUMP") {
+            if (command_parts.size() < 2) {
+                throw std::runtime_error("Incorrect JUMP command. Usage: JUMP <line num>");
+            }
+            int jump_line = stoi(command_parts[1]);
+            file.clear();
+            file.seekg(0);
+            for (int i = 0; i < jump_line; i++) {
+                getline(file, line);
+            }
+            current_line = jump_line - 1;
+        }
+
+        else if (command == "JZ") {
+            if (command_parts.size() < 2) {
+                throw std::runtime_error("Incorrect JZ command. Usage: JZ <line num>");
+            }
+            int jump_line = stoi(command_parts[1]);
+            if (std::get<int>(stack.back()) == 0) {
+                file.clear();
+                file.seekg(0);
+                for (int i = 0; i < jump_line; i++) {
+                    getline(file, line);
+                }
+                current_line = jump_line - 1;
+            }
+        }
+
+        else if (command == "JNZ") {
+            if (command_parts.size() < 2) {
+                throw std::runtime_error("Incorrect JNZ command. Usage: JNZ <line num>");
+            }
+            int jump_line = stoi(command_parts[1]);
+            if (std::get<int>(stack.back()) != 0) {
+                file.clear();
+                file.seekg(0);
+                for (int i = 0; i < jump_line; i++) {
+                    getline(file, line);
+                }
+                current_line = jump_line - 1;
+            }
         }
     }
 
